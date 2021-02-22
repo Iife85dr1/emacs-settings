630627400062dd5665ebb3db3c5dd4e29abc37c4
John Doe <you@example.com>
2021-02-22 02:44:26 +0100

    ruby mode lsp

--- langs/lsp.el
+++ langs/lsp.el
@@ -13,6 +13,7 @@
          (vue-mode . lsp)
          (web-mode . lsp)
          (rustic-mode . lsp)
+         (ruby-mode . lsp)
          ;; if you want which-key integration
          (lsp-mode . lsp-enable-which-key-integration))
   :config
